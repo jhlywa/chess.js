b2fccd1f7b5b8cc3dde2ff075c774b8f99bf51a8
Jeff Hlywa <jhlywa@gmail.com>
2010-10-07 17:14:50 -0400

    Fixed broken get/put test.

--- tests/tests-browser.js
+++ tests/tests-browser.js
@@ -194,7 +194,18 @@ function get_and_put_tests() {
     /* iterate over every square to make sure get returns the proper
      * piece values/color 
      */
-    for (var square in Chess().SQUARES) {
+    var squares = [
+      'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
+      'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
+      'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6',
+      'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5',
+      'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4',
+      'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3',
+      'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2',
+      'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'];
+
+    for (var j = 0; j < squares.length; j++) {
+      var square = squares[j];
       if (!(square in positions[i].pieces)) {
         if (chess.get(square) != null) {
           passed = false;
@@ -211,6 +222,7 @@ function get_and_put_tests() {
 
     log(s);
   }
+
   var finish = new Date();
   var diff = (finish - start) / 1000;
   log('--> Get/Put Time: ' + diff + ' secs');
