cfe1db0731a71f3fe42cd0feb05b769120a191f9
Jeff Hlywa <jhlywa@gmail.com>
2010-10-07 16:46:27 -0400

    Moved tests into a 'tests' directory.

--- tests/tests-browser.js
+++ tests/tests-browser.js
@@ -264,7 +264,7 @@ function make_move_tests() {
     chess.load(positions[i].fen);
     var s = 'Make Move Test #' + i + ': ' + positions[i].fen + 
             ' (' + positions[i].move + ' ' + positions[i].legal + ') : ';
-    result = chess.make_move(positions[i].move);
+    result = chess.move(positions[i].move);
     if (positions[i].legal) {
       s += (result && chess.fen() == positions[i].next) ? 'PASSED!' : 'FAILED!';
     } else {
