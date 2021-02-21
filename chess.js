ca4de34f5ed00f9a8b3c0d96ff16a9144923a514
Jeff Hlywa <jhlywa@gmail.com>
2021-02-21 09:54:44 -0500

    Update copyright year

--- chess.js
+++ chess.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Jeff Hlywa (jhlywa@gmail.com)
+ * Copyright (c) 2021, Jeff Hlywa (jhlywa@gmail.com)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
