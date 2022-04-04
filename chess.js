aa0fa5b8ba1e2e40b68413f4e6fcd249002a74b3
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2022-04-04 12:31:12 -0400

    attacked() calls attack_count()

--- chess.js
+++ chess.js
@@ -899,50 +899,7 @@ export const Chess = function (fen) {
   }
 
   function attacked(color, square) {
-    for (var i = SQUARE_MAP.a8; i <= SQUARE_MAP.h1; i++) {
-      /* did we run off the end of the board */
-      if (i & 0x88) {
-        i += 7
-        continue
-      }
-
-      /* if empty square or wrong color */
-      if (board[i] == null || board[i].color !== color) continue
-
-      var piece = board[i]
-      var difference = i - square
-      var index = difference + 119
-
-      if (ATTACKS[index] & (1 << SHIFTS[piece.type])) {
-        if (piece.type === PAWN) {
-          if (difference > 0) {
-            if (piece.color === WHITE) return true
-          } else {
-            if (piece.color === BLACK) return true
-          }
-          continue
-        }
-
-        /* if the piece is a knight or a king */
-        if (piece.type === 'n' || piece.type === 'k') return true
-
-        var offset = RAYS[index]
-        var j = i + offset
-
-        var blocked = false
-        while (j !== square) {
-          if (board[j] != null) {
-            blocked = true
-            break
-          }
-          j += offset
-        }
-
-        if (!blocked) return true
-      }
-    }
-
-    return false
+    return attack_count(color, square) >= 1
   }
 
   function attack_count(color, square) {
