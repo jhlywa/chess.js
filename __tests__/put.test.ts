97241ae0c67ffd974114bef896513d3a43499d95
William Hoggarth <mail@whoggarth.org>
2023-04-23 13:57:31 +0200

    Ensure put/remove functions update en passant square (#398)

--- __tests__/put.test.ts
+++ __tests__/put.test.ts
@@ -7,6 +7,7 @@ import {
   BLACK,
   PAWN,
   KNIGHT,
+  BISHOP,
   ROOK,
   KING,
 } from '../src/chess'
@@ -110,4 +111,74 @@ test('put - replacing black king loses castling rights', () => {
   chess.put({ type :KNIGHT, color: BLACK}, 'e8')
   expect(chess.moves()).not.toContain('O-O')
   expect(chess.moves()).not.toContain('O-O-O')
+});
+
+test('put - replacing white pawn clears en passant square', () => {
+  const chess = new Chess('rnbqkbnr/pppppp1p/8/8/3PPPp1/8/PPP3PP/RNBQKBNR b KQkq f3 0 3')
+
+  chess.put({ type :KNIGHT, color: WHITE}, 'f4')
+  expect(chess.moves()).not.toContain('gxf3')
+});
+
+test('put - occupying white en passant square clears it', () => {
+  const chess = new Chess('rnbqkbnr/pppppp1p/8/8/3PPPp1/8/PPP3PP/RNBQKBNR b KQkq f3 0 3')
+
+  chess.put({ type :KNIGHT, color: BLACK}, 'f3')
+  expect(chess.moves()).not.toContain('gxf3')
+});
+
+test('put - occupying white starting square clears en passant square', () => {
+  const chess = new Chess('rnbqkbnr/pppppp1p/8/8/3PPPp1/8/PPP3PP/RNBQKBNR b KQkq f3 0 3')
+
+  chess.put({ type :KNIGHT, color: WHITE}, 'f2')
+  expect(chess.moves()).not.toContain('gxf3')
+});
+
+test('put - replacing black pawn clears white en passant square 1', () => {
+  const chess = new Chess('rnbqkbnr/pppppp1p/8/8/3PPPp1/8/PPP3PP/RNBQKBNR b KQkq f3 0 3')
+
+  chess.put({ type :BISHOP, color: BLACK}, 'g4')
+  expect(chess.moves()).not.toContain('gxf3')
+});
+
+test('put - replacing black pawn clears white en passant square 2', () => {
+  const chess = new Chess('rnbqkbnr/p1pppppp/8/8/1pPPP3/8/PP3PPP/RNBQKBNR b KQkq c3 0 3')
+
+  chess.put({ type :BISHOP, color: BLACK}, 'b4')
+  expect(chess.moves()).not.toContain('bxc3')
+});
+
+test('put - replacing black pawn clears en passant square', () => {
+  const chess = new Chess('rnbqkbnr/pppp2pp/8/4ppP1/8/8/PPPPPP1P/RNBQKBNR w KQkq f6 0 3')
+
+  chess.put({ type :KNIGHT, color: BLACK}, 'f5')
+  expect(chess.moves()).not.toContain('gxf6')
+});
+
+test('put - occupying black en passant square clears it', () => {
+  const chess = new Chess('rnbqkbnr/pppp2pp/8/4ppP1/8/8/PPPPPP1P/RNBQKBNR w KQkq f6 0 3')
+
+  chess.put({ type :KNIGHT, color: WHITE}, 'f6')
+  expect(chess.moves()).not.toContain('gxf6')
+});
+
+test('put - occupying black starting square clears en passant square', () => {
+  const chess = new Chess('rnbqkbnr/pppp2pp/8/4ppP1/8/8/PPPPPP1P/RNBQKBNR w KQkq f6 0 3')
+
+  chess.put({ type :KNIGHT, color: BLACK}, 'f7')
+  expect(chess.moves()).not.toContain('gxf6')
+});
+
+test('put - replacing white pawn clears black en passant square 1', () => {
+  const chess = new Chess('rnbqkbnr/pppp2pp/8/4ppP1/8/8/PPPPPP1P/RNBQKBNR w KQkq f6 0 3')
+
+  chess.put({ type :BISHOP, color: WHITE}, 'g5')
+  expect(chess.moves()).not.toContain('gxf6')
+});
+
+test('put - replacing white pawn clears black en passant square 2', () => {
+  const chess = new Chess('rnbqkbnr/pp2pppp/8/1Ppp4/8/8/P1PPPPPP/RNBQKBNR w KQkq c6 0 3')
+
+  chess.put({ type :BISHOP, color: WHITE}, 'b5')
+  expect(chess.moves()).not.toContain('bxc6')
 });
--- __tests__/remove.test.ts
+++ __tests__/remove.test.ts
@@ -1,4 +1,4 @@
-import { Chess, Square, QUEEN, WHITE } from '../src/chess'
+import { Chess, Square, QUEEN, WHITE, KNIGHT, BISHOP, BLACK } from '../src/chess'
 
 test('remove - returns piece', () => {
   const chess = new Chess()
@@ -58,4 +58,46 @@ test('remove - removing black king loses castling rights', () => {
   chess.remove( 'e8')
   expect(chess.moves()).not.toContain('O-O')
   expect(chess.moves()).not.toContain('O-O-O')
+});
+
+test('remove - removing white pawn clears en passant square', () => {
+  const chess = new Chess('rnbqkbnr/pppppp1p/8/8/3PPPp1/8/PPP3PP/RNBQKBNR b KQkq f3 0 3')
+
+  chess.remove('f4')
+  expect(chess.moves()).not.toContain('gxf3')
+});
+
+test('remove - removing black pawn clears white en passant square 1', () => {
+  const chess = new Chess('rnbqkbnr/pppppp1p/8/8/3PPPp1/8/PPP3PP/RNBQKBNR b KQkq f3 0 3')
+
+  chess.remove('g4')
+  expect(chess.moves()).not.toContain('gxf3')
+});
+
+test('remove - removing black pawn clears white en passant square 2', () => {
+  const chess = new Chess('rnbqkbnr/p1pppppp/8/8/1pPPP3/8/PP3PPP/RNBQKBNR b KQkq c3 0 3')
+
+  chess.remove('b4')
+  expect(chess.moves()).not.toContain('bxc3')
+});
+
+test('remove - removing black pawn clears en passant square', () => {
+  const chess = new Chess('rnbqkbnr/pppp2pp/8/4ppP1/8/8/PPPPPP1P/RNBQKBNR w KQkq f6 0 3')
+
+  chess.remove('f5')
+  expect(chess.moves()).not.toContain('gxf6')
+});
+
+test('remove - removing white pawn clears black en passant square 1', () => {
+  const chess = new Chess('rnbqkbnr/pppp2pp/8/4ppP1/8/8/PPPPPP1P/RNBQKBNR w KQkq f6 0 3')
+
+  chess.remove('g5')
+  expect(chess.moves()).not.toContain('gxf6')
+});
+
+test('remove - removing white pawn clears black en passant square 2', () => {
+  const chess = new Chess('rnbqkbnr/pp2pppp/8/1Ppp4/8/8/P1PPPPPP/RNBQKBNR w KQkq c6 0 3')
+
+  chess.remove('b5')
+  expect(chess.moves()).not.toContain('bxc6')
 });
