8d71c7e949979edc3fbbc4557d5427d6a437775e
William Hoggarth <mail@whoggarth.org>
2025-05-18 12:28:59 +0100

    Basic hash tests

--- __tests__/zobrist.test.ts
+++ __tests__/zobrist.test.ts
@@ -1,4 +1,6 @@
-import { xoroshiro128 } from '../src/chess'
+import { Chess, xoroshiro128 } from '../src/chess'
+
+// Very basic hash tests, more extensive testing is done as part of the move and load pgn tests.
 
 test('rand', () => {
   const rand = xoroshiro128(0xa187eb39cdcaed8f31c4b365b102e01en)
@@ -13,4 +15,21 @@ test('rand', () => {
     '5f29a08525b0575f',
     'f6718179e0f7ba94',
   ])
+})
+
+test('hash is the same for the same position', () => {
+  const a = new Chess()
+  const b = new Chess()
+
+  expect(a.hash()).toEqual(b.hash())
+})
+
+test('hash is different for different positions', () => {
+  const a = new Chess()
+  a.move('e4')
+
+  const b = new Chess()
+  b.move('d4')
+
+  expect(a.hash()).not.toEqual(b.hash())
 })
